18535fca787867552cdb13837a95cdec7214d064
Marcel Former <34310427+Selians@users.noreply.github.com>
2022-11-05 23:32:09 +0100

    Mod updates

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -3,7 +3,13 @@
 console.info('Hello, World! (You will see this line every time server resources reload)')
 
 ServerEvents.recipes(event => {
+
+	/* REMOVE BY RECIPE ID */
 	event.remove({id:'projectvibrantjourneys:groundcover_cobblestone'});
+
+	/* REMOVE BY MODID */
+	event.remove({mod:'arclight'});
+	
 })
 
 ServerEvents.tags('item', event => {
@@ -12,4 +18,15 @@ ServerEvents.tags('item', event => {
 
 	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
 	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
+})
+
+
+LootJS.modifiers((event) => {
+	event
+		.addEntityLootModifier("ob_aquamirae:captain_cornelia")
+		.addLoot("endrem:coldeye");
+
+	event
+		.addEntityLootModifier("cataclysm:netherite_monstrosity")
+		.addLoot("endrem:nether_eye");
 })
